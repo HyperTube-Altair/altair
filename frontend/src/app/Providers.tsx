51758cda25054ab0a0d8630f654986fadb846ed6
spicyi <del-khay@student.1337.ma>
2024-08-21 18:53:05 +0100

    setup next ui

---
+++ frontend/src/app/Providers.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import { NextUIProvider } from "@nextui-org/react";
+import { ReactNode } from "react";
+
+const Providers = ({ children }: {children : ReactNode}) => {
+    return (
+        <NextUIProvider>
+            {children}
+        </NextUIProvider>
+    );
+};
+export default Providers;
