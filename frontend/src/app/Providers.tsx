2c8a98d6db80c605bb17ff00b84a5bd950272f33
Saturn Lotus <112328616+Hattaki-Hajar@users.noreply.github.com>
2024-08-25 10:33:02 +0100

    add redux toolkit (#9)

--- frontend/src/app/Providers.tsx
+++ frontend/src/app/Providers.tsx
@@ -2,8 +2,16 @@
 
 import { NextUIProvider } from '@nextui-org/react';
 import { ReactNode } from 'react';
+import { store } from '@/app/store'
+import { Provider as ReduxProvider } from 'react-redux'
 
 const Providers = ({ children }: { children: ReactNode }) => {
-	return <NextUIProvider>{children}</NextUIProvider>;
+	return (
+		<NextUIProvider>
+			<ReduxProvider store={store}>
+				{children}
+			</ReduxProvider>
+		</NextUIProvider>	
+	);
 };
 export default Providers;
