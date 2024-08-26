afb6da651a4107400eebcff00a215986c191a1a9
spicyi <del-khay@student.1337.ma>
2024-08-26 15:08:18 +0100

    refactor: Update webpack configuration to include source maps in development mode

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+	webpack(config, { dev }) {
+		if (dev) {
+			config.devtool = 'source-map';
+		}
+		return config;
+	},
+	images: { unoptimized: true },
+};
 
 export default nextConfig;
