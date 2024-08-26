b97a6c408e37db08466701bc32cb9df4459e5ae3
spicyi <del-khay@student.1337.ma>
2024-08-26 16:47:24 +0100

    chore: Update VideoCard component styling for better layout and button alignment

--- frontend/src/app/component/VideoCard.tsx
+++ frontend/src/app/component/VideoCard.tsx
@@ -18,31 +18,25 @@ export const VideoCard = () => {
 		return stars;
 	};
 	return (
-		
-		<Card
-			isFooterBlurred
-			fullWidth={false}
-			radius="lg"
-			className=" relative border-none h-80 w-72">
-			<Image className="object-cover" src="https://picsum.photos/288/320" height={384} width={288} />
+		<Card isFooterBlurred fullWidth={false} radius="sm" className=" relative h-64 w-40">
+			<Image className="object-cover" src="https://picsum.photos/160/256" height={256} width={160} />
 			<Tooltip content={`${rating}/5`} placement="top" size="sm">
 				<div className="absolute flex z-10 top-1 right-1">{getRating(rating)}</div>
 			</Tooltip>
-			<CardFooter className="justify-between before:bg-white/10 border-white/20 border-1 overflow-hidden py-1 absolute before:rounded-xl rounded-large bottom-1 w-[calc(100%_-_8px)] shadow-small ml-1 z-10">
+			<CardFooter className="justify-between flex-col before:bg-white/10 border-white/20 border-1 overflow-hidden py-1 absolute before:rounded-xl rounded-large bottom-1 w-[calc(100%_-_8px)] shadow-small ml-1 z-10">
 				<div>
 					<p className="text-tiny text-white/80">Name of the movies</p>
 					<div className="flex items-center justify-start gap-1">
 						<p className="text-tiny text-white/60">2024</p>
 					</div>
 				</div>
-				<div className="flex gap-1">
+				<div className="flex items-center justify-center w-full">
 					<Tooltip content="Watch later" placement="top">
 						<Button
 							className="text-tiny text-white bg-black/20"
 							variant="flat"
 							color="default"
 							isIconOnly
-							radius="lg"
 							size="sm">
 							<ClockIcon className="w-4 h-4" />
 						</Button>
@@ -53,7 +47,6 @@ export const VideoCard = () => {
 							variant="flat"
 							color="default"
 							isIconOnly
-							radius="lg"
 							size="sm">
 							<PlusCircleIcon className="w-4 h-4" />
 						</Button>
@@ -64,7 +57,6 @@ export const VideoCard = () => {
 							variant="flat"
 							color="default"
 							isIconOnly
-							radius="lg"
 							size="sm">
 							<HeartIcon className="w-4 h-4" />
 						</Button>
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,34 +2,51 @@
 
 import React from 'react';
 
-import { NavBar } from '@/app/component/NavBar';
 import { RectangleGroupIcon } from '@heroicons/react/16/solid';
 import { VideoCard } from './component/VideoCard';
+import { Button, Spacer } from '@nextui-org/react';
+import { PlusCircleIcon, TicketIcon } from '@heroicons/react/24/outline';
+import { CardSilder } from './component/CardSilder';
 
 export default function Home() {
 	return (
-		<main className=" flex  min-h-screen flex-col items-center  bg-[url('/main.jpg')] bg-top bg-contain bg-no-repeat -mt-16">
-			{/* <div className="absolute inset-0 bg-gradient-to-r from-black from-2%" /> */}
-			<div className="flex  items-center justify-center w-full h-1/2 min-h-[400px] max-h-[436px] mt-16">test</div>
-			<div className="flex flex-col w-full p-4 space-y-2">
+		<main className="flex  min-h-screen flex-col items-center  bg-[url('/main.jpg')] bg-secondary-200/30 bg-top  bg-contain bg-no-repeat -mt-16">
+			<div className="absolute z-[1] inset-0 bg-gradient-to-bl from-transparent to-90% via-black/15  to-secondary-200/30" />
+			<div className="flex flex-col z-10 items-start justify-center  h-1/2 w-2/5 min-h-[400px] max-h-[436px] mt-16  self-start p-4 gap-3">
+				<h1 className=" text-6xl font-bold text-start text-white capitalize">big movies name here</h1>
+				<div className="felx gap-4 mt-8">
+					<span className=" ml-2 text-gray-300 pr-3"> Remarks Here </span>
+					<span className="text-green-500 text-md font-bold">92% Match </span>
+					<span className="text-white text-md font-bold">2021</span>
+				</div>
+				<div className="flex gap-4 self-start items-center w-full ">
+					<Button color="secondary" size="lg" className="bg-secondary-300 w-1/2" radius="full">
+						<span className=" capitalize font-bold">PLAY</span>
+					</Button>
+					<Button size="lg" radius="full" isIconOnly className="bg-transparent">
+						<PlusCircleIcon className="w-14  text-gray-100" />
+					</Button>
+				</div>
+			</div>
+			<div className="flex z-10 flex-col w-full p-4 space-y-2">
 				<h1 className="text-xl font-bold text-center flex gap-2  ">
 					<RectangleGroupIcon className="w-7 h-7 text-secondary" />
 					Recommended
 				</h1>
-				<div className="flex gap-4 flex-wrap justify-center">
-					<VideoCard />
-					<VideoCard />
-					<VideoCard />
-					<VideoCard />
-					<VideoCard />
-					<VideoCard />
-					<VideoCard />
-					<VideoCard />
-					<VideoCard />
-					<VideoCard />
-					<VideoCard />
-					<VideoCard />
-					<VideoCard />
+				<CardSilder>
+					{Array.from({ length: 10 }).map((_, i) => (
+						<VideoCard key={i} />
+					))}
+				</CardSilder>
+				<Spacer y={2} />
+				<h1 className="text-xl font-bold text-center flex gap-2  ">
+					<TicketIcon className="w-7 h-7 text-secondary" />
+					New Releases
+				</h1>
+				<div className="grid grid-cols-8 place-items-center gap-2">
+					{Array.from({ length: 13 }).map((_, i) => (
+						<VideoCard key={i} />
+					))}
 				</div>
 			</div>
 		</main>
