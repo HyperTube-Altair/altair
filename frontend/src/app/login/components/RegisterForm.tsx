671deefc9bdee915ac1b482c4f03a8bacac21a5a
Hattaki-Hajar_bcgprod <Hattaki.Hajar@bcg.com>
2024-12-14 00:15:43 +0100

    Feat: Refactor login and registration forms into separate components

---
+++ frontend/src/app/login/components/RegisterForm.tsx
@@ -0,0 +1,92 @@
+'use client';
+import React from 'react'
+import { Input, Button } from '@nextui-org/react';
+import { LockClosedIcon, UserIcon } from '@heroicons/react/16/solid';
+
+const RegisterForm = () => {
+  return (
+	<div className='w-full'>
+		<form className="flex flex-col h-full w-full gap-2" onSubmit={() => alert('Submitted!')}>
+			<span className="text-3xl font-bold text-gray-300 flex justify-center">Register</span>
+			<div className='flex gap-2'>
+				<Input
+					type="text"
+					variant="bordered"
+					radius="full"
+					size='sm'
+					label="First name"
+					classNames={{ label: 'text-white' }}
+					endContent={<UserIcon className="w-6 text-white self-center" />}
+					/>
+				<Input
+					type="text"
+					variant="bordered"
+					radius="full"
+					label="Last name"
+					size='sm'
+					classNames={{ label: 'text-white' }}
+					endContent={<UserIcon className="w-6 text-white self-center" />}
+				/>
+			</div>
+			<Input
+				type="text"
+				variant="bordered"
+				radius="full"
+				label="Username"
+				size='sm'
+				classNames={{ label: 'text-white' }}
+				endContent={<UserIcon className="w-6 text-white self-center" />}
+			/>
+			<Input
+				type="email"
+				variant="bordered"
+				radius="full"
+				label="Email"
+				size='sm'
+				classNames={{ label: 'text-white' }}
+				endContent={<UserIcon className="w-6 text-white self-center" />}
+			/>
+			<div className='flex gap-2'>
+				<Input
+					type="password"
+					variant="bordered"
+					radius="full"
+					label="Password"
+					size='sm'
+					classNames={{ label: 'text-white' }}
+					endContent={<LockClosedIcon className="w-6 text-white self-center" />}
+				/>
+				<Input
+					type="password"
+					variant="bordered"
+					radius="full"
+					label="Confirm password"
+					size='sm'
+					classNames={{ label: 'text-white' }}
+					endContent={<LockClosedIcon className="w-6 text-white self-center" />}
+				/>
+			</div>
+			<Button size="lg" className="w-full" radius="full">
+				Register
+			</Button>
+			<span className="flex flex-col items-center text-gray-400">Or</span>
+			<div className="bg-white/20 rounded-full p-1 flex items-center space-x-3">
+				<img
+					src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTXfAZMOWHDQ3DKE63A9jWhIqQaKcKqUIXvzg&s"
+					className="w-8 rounded-full"
+				/>
+				<span className="text-gray-300">Register with 42</span>
+			</div>
+			<div className="bg-white/15 rounded-full p-1 flex items-center space-x-3">
+				<img
+					src="https://w7.pngwing.com/pngs/63/1016/png-transparent-google-logo-google-logo-g-suite-chrome-text-logo-chrome.png"
+					className="w-8 rounded-full"
+				/>
+				<span className="text-gray-300">Register with google</span>
+			</div>
+		</form>
+	</div>
+  )
+}
+
+export default RegisterForm
--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -1,101 +1,19 @@
 'use client';
-import { Card, CardBody, Input, Button, Tabs, Tab } from '@nextui-org/react';
-import { LockClosedIcon, UserIcon } from '@heroicons/react/16/solid';
+import { Card, CardBody, Tabs, Tab } from '@nextui-org/react';
+import LoginForm from './components/LoginForm';
+import RegisterForm from './components/RegisterForm';
 
 const Page = () => {
 	return (
 		<main className='w-screen h-screen flex items-center justify-center bg-[url("https://r4.wallpaperflare.com/wallpaper/94/797/206/kimi-no-na-wa-makoto-shinkai-starry-night-comet-wallpaper-89c0584d016a0d0be617288fb071564d.jpg")] bg-no-repeat bg-center bg-cover'>
-			<Card className="w-[450px] max-w-[90%] min-w-[260px] h-[550px] flex items-center px-2 bg-white/5" isBlurred>
-				<CardBody className="space-y-4 flex items-center justify-start h-full w-3/4 mt-6">
-					<Tabs key={'authentication tabs'} color="secondary" radius="full" size="lg" fullWidth>
-						<Tab key="login" title="Login">
-							<form className="flex flex-col h-full w-full gap-2">
-								<span className="text-3xl font-bold text-gray-300 flex justify-center">Login</span>
-								<Input
-									type="email"
-									variant="bordered"
-									radius="full"
-									label="Username"
-									classNames={{ label: 'text-white' }}
-									endContent={<UserIcon className="w-6 text-white self-center" />}
-								/>
-								<Input
-									type="password"
-									variant="bordered"
-									radius="full"
-									label="Password"
-									classNames={{ label: 'text-white' }}
-									endContent={<LockClosedIcon className="w-6 text-white self-center" />}
-								/>
-								<span className="text-gray-400 w-full flex-row text-end hover:text-blue-500 cursor-pointer">
-									Forgot password?
-								</span>
-								<Button size="lg" className="w-full" radius="full">
-									Login
-								</Button>
-								<span className="flex flex-col items-center text-gray-400">Or</span>
-								<div className="bg-white/20 rounded-full p-1 flex items-center space-x-3">
-									<img
-										src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTXfAZMOWHDQ3DKE63A9jWhIqQaKcKqUIXvzg&s"
-										className="w-8 rounded-full"
-									/>
-									<span className="text-gray-300">Login with 42</span>
-								</div>
-								<div className="bg-white/15 rounded-full p-1 flex items-center space-x-3">
-									<img
-										src="https://w7.pngwing.com/pngs/63/1016/png-transparent-google-logo-google-logo-g-suite-chrome-text-logo-chrome.png"
-										className="w-8 rounded-full"
-									/>
-									<span className="text-gray-300">Login with google</span>
-								</div>
-							</form>
+			<Card className="w-[450px] max-w-[90%] min-w-[260px] h-[600px] flex items-center px-2 bg-white/5" isBlurred>
+				<CardBody className="space-y-4 flex items-center justify-start h-full w-full mt-6">
+					<Tabs key={'authentication tabs'} color="secondary" radius="full" size="lg" fullWidth className='w-full'>
+						<Tab key="login" title="Login" className='w-full'>
+							<LoginForm />
 						</Tab>
-						<Tab key="register" title="Register">
-							<form className="flex flex-col h-full w-full gap-2" onSubmit={() => alert('Submitted!')}>
-								<span className="text-3xl font-bold text-gray-300 flex justify-center">Register</span>
-								<Input
-									type="email"
-									variant="bordered"
-									radius="full"
-									label="Username"
-									classNames={{ label: 'text-white' }}
-									endContent={<UserIcon className="w-6 text-white self-center" />}
-								/>
-								<Input
-									type="password"
-									variant="bordered"
-									radius="full"
-									label="Password"
-									classNames={{ label: 'text-white' }}
-									endContent={<LockClosedIcon className="w-6 text-white self-center" />}
-								/>
-								<Input
-									type="password"
-									variant="bordered"
-									radius="full"
-									label="Confirm password"
-									classNames={{ label: 'text-white' }}
-									endContent={<LockClosedIcon className="w-6 text-white self-center" />}
-								/>
-								<Button size="lg" className="w-full" radius="full">
-									Register
-								</Button>
-								<span className="flex flex-col items-center text-gray-400">Or</span>
-								<div className="bg-white/20 rounded-full p-1 flex items-center space-x-3">
-									<img
-										src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTXfAZMOWHDQ3DKE63A9jWhIqQaKcKqUIXvzg&s"
-										className="w-8 rounded-full"
-									/>
-									<span className="text-gray-300">Register with 42</span>
-								</div>
-								<div className="bg-white/15 rounded-full p-1 flex items-center space-x-3">
-									<img
-										src="https://w7.pngwing.com/pngs/63/1016/png-transparent-google-logo-google-logo-g-suite-chrome-text-logo-chrome.png"
-										className="w-8 rounded-full"
-									/>
-									<span className="text-gray-300">Register with google</span>
-								</div>
-							</form>
+						<Tab key="register" title="Register" className='w-full'>
+							<RegisterForm />
 						</Tab>
 					</Tabs>
 				</CardBody>
