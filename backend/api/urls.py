5133d73beeea9ac560cef0b476148a831c86272a
spicyi <del-khay@student.1337.ma>
2024-12-23 19:14:12 +0100

    feat: search engine app

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,7 +1,10 @@
-from django.urls import path
+from django.urls import path, include
 
 import api.views as views
+import search_engine.urls as search_engine_urls
+
 
 urlpatterns = [
     path("", views.api_root, name="api_root"),
+    path("search/", include(search_engine_urls), name="search"),
 ]
---
+++ backend/search_engine/urls.py
@@ -0,0 +1,2 @@
+from django.urls import path  # noqa: F401
+
