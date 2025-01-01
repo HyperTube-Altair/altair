032f72bbdd685930322bfc11142bbcd46403901d
BCGx <bcgx@Anasss-MacBook-Pro-2.local>
2025-01-01 18:36:16 +0100

    code format needed prettier

--- frontend/src/app/movie/comment.tsx
+++ frontend/src/app/movie/comment.tsx
@@ -1,19 +1,16 @@
-import { Avatar } from "@nextui-org/avatar";
-import { Card, CardBody, CardHeader } from "@nextui-org/card";
+import { Avatar } from '@nextui-org/avatar';
+import { Card, CardBody, CardHeader } from '@nextui-org/card';
 
 export default function Comment() {
-  return (
-    <div className="w-[80%] h-fit">
-      <Card className="w-full">
-        <CardHeader className="flex flex-row gap-[10px] font-bold">
-          <Avatar
-            src="https://i.pravatar.cc/300"
-            className="w-[48px] h-[48px]"
-          ></Avatar>
-          Houssam-eddine Moumanis
-        </CardHeader>
-        <CardBody>Yaaah wlh ila Driss wtf!!</CardBody>
-      </Card>
-    </div>
-  );
+	return (
+		<div className="w-[80%] h-fit">
+			<Card className="w-full">
+				<CardHeader className="flex flex-row gap-[10px] font-bold">
+					<Avatar src="https://i.pravatar.cc/300" className="w-[48px] h-[48px]"></Avatar>
+					Houssam-eddine Moumanis
+				</CardHeader>
+				<CardBody>Yaaah wlh ila Driss wtf!!</CardBody>
+			</Card>
+		</div>
+	);
 }
