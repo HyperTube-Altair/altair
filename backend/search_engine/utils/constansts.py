eb2c3e8c82b56392fae9d618efac29607e13c115
spicyi <del-khay@student.1337.ma>
2025-01-04 13:09:06 +0100

    feat: enhance search functionality with new endpoints and serializers for trending and genre retrieval

--- backend/search_engine/utils/constansts.py
+++ backend/search_engine/utils/constansts.py
@@ -1,8 +1,7 @@
 import os
-from typing import Optional
 
 TMDP_BASE_URL = "https://api.themoviedb.org/3"
 
 
 class Env:
-    TMDP_API_KEY: Optional[str] = os.environ.get("TMDP_API_KEY", None)
+    TMDP_API_KEY: str = os.environ.get("TMDP_API_KEY")
