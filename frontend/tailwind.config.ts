8010bdbe53741208534e49c66274e58618c2134d
BCGx <bcgx@Anasss-MacBook-Pro-2.local>
2025-01-05 14:18:44 +0100

    [CONFIG] updated nextui && added color to tailwing config

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -16,6 +16,9 @@ const config: Config = {
 				'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
 			},
 		},
+		colors: {
+			'second-white': 'rgba(255,255,255,0.87)',
+		},
 	},
 	darkMode: 'class',
 	plugins: [nextui()],
