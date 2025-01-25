aafef98308c2aba980718d855db6a5af0879b11e
Hattaki-Hajar <hattakihajar28@gmail.com>
2025-01-25 11:53:14 +0100

    Fix: Fix CI error

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
 import Providers from './Providers';
-import { NavBar } from './component/NavBar';
 
 const inter = Inter({ subsets: ['latin'] });
 
